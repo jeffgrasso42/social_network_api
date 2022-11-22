b07b75c346b329e8f96f3753b6737ac09de9bbde
JAG <jagrasso42@gmail.com>
2022-11-22 17:11:11 -0500

    added middleware

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const routes = require('./routes');
 
 const PORT = process.env.port || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
