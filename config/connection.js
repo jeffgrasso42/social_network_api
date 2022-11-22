158443dad9a0b5039efab74d30254ed6eda683df
JAG <jagrasso42@gmail.com>
2022-11-22 17:08:59 -0500

    switched to localhost server

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,8 @@
-require('dotenv').config();
 const { connect, connection } = require('mongoose');
 
-connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.25doxpy.mongodb.net/?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+connect('mongodb://localhost/socialnetworkDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = connection;
