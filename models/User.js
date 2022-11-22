1445445c92c766323774b67aa4195e0b18f69956
JAG <jagrasso42@gmail.com>
2022-11-22 17:54:22 -0500

    adds userSchema

---
+++ models/User.js
@@ -0,0 +1,29 @@
+// DEPENDENCIES
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+const re = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,20})$/;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: re,
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
