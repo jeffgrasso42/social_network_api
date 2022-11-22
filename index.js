0149f23d961af8bc317e08a80adf50ce7a6d6fb8
JAG <jagrasso42@gmail.com>
2022-11-22 17:14:19 -0500

    server added

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const routes = require('./routes');
 const PORT = process.env.port || 3001;
 const app = express();
 
+// MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+// SERVER
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Social Network API server running on port ${PORT}!`);
+  });
+});
