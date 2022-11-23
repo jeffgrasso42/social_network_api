a37172fd175f506217e824bc7a7695926c68da0a
JAG <jagrasso42@gmail.com>
2022-11-22 19:41:42 -0500

    adds routers exports and imports

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+module.exports = router;
