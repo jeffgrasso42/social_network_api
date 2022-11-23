c9e2dfbc63abdecd6eebda53b0d0388c5da150c7
JAG <jagrasso42@gmail.com>
2022-11-22 19:50:50 -0500

    adds endpoints for userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,4 +9,30 @@ const {
   removeFriend,
 } = require('../../controllers/userController');
 
+// /api/users
+router
+  .route('/')
+  // GET - all users
+  .get(getUsers)
+  // POST - create a user
+  .post(createUser);
+
+// /api/users/"userId"
+router
+  .route('/:userId')
+  // GET - get one user by ID
+  .get(getSingleUser)
+  // PUT - update a user by ID
+  .put(updateUser)
+  // DELETE - delete a user by ID
+  .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router
+  .route('/:userId/friends/:friendId')
+  // PUT - add a friendId to the users friend array
+  .put(addFriend)
+  // DELETE - remove a friendId from the user's friend array
+  .delete(removeFriend);
+
 module.exports = router;
