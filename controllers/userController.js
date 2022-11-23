91cb5b5dbc65016b6c3f46ce30aa34b7fa60b511
JAG <jagrasso42@gmail.com>
2022-11-22 19:13:49 -0500

    adds controller to get a single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,4 +12,12 @@ module.exports = {
       })
       .catch(err => res.status(500).json(err));
   },
+  // Get one user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('thoughts')
+      .then(user => (!user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user)))
+      .catch(err => res.status(500).json(err));
+  },
 };
