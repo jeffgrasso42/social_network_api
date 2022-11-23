46352c8c18542499e50d453a1a87e4c310838eb1
JAG <jagrasso42@gmail.com>
2022-11-23 13:59:04 -0500

    adds controller function for adding a reaction to a thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -80,4 +80,20 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  // Add a reaction
+  async addReaction(req, res) {
+    try {
+      const updatedThought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      !updatedThought ? res.status(404).json({ message: 'No thought with this id!' }) : res.json(updatedThought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 };
