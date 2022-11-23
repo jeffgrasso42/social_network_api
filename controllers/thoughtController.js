4ddb75653e6d1751375869e6abeaf7533639daf7
JAG <jagrasso42@gmail.com>
2022-11-22 20:19:34 -0500

    adds controller function to get all thoughts from DB

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,5 +3,13 @@ const { User, Thought } = require('../models');
 
 module.exports = {
   // Get all thoughts
-  getThoughts,
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find().sort({ createdAt: -1 });
+      res.json(thoughts);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 };
