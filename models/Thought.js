4984a832faf73f28f79942bba705b2ce3f72f5c6
JAG <jagrasso42@gmail.com>
2022-11-26 11:54:50 -0500

    added options and virtual to thoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -2,23 +2,35 @@ const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction.js');
 const date = require('../utils/date.js');
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    min: 1,
-    max: 280,
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: timestamp => date(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    get: timestamp => date(timestamp),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
 });
 
 const Thought = model('thought', thoughtSchema);
